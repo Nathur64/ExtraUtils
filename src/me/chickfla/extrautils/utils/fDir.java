2ec6238340c68841b153cca647cc779d4c20dfe8
Nathur64 <36383868+Nathur64@users.noreply.github.com>
2019-04-08 15:13:21 -0500

    Create fDir.java
    We will need folder-detection and file-detection for our plugin to work.  I don't know how to write it in java, but I can write it in kotlin.

---
+++ src/me/chickfla/extrautils/utils/fDir.java
@@ -0,0 +1,4 @@
+package me.chickfla.extrautils.utils;
+public class fDir {
+    //ToDo:   Write methods that detect files within the server's directory.  This will allow our plugin to detect what permission-plugin is being used, and later we may be using this to help this plugin avoid errors.  
+};
