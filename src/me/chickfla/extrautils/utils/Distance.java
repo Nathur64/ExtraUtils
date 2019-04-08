20070d1f250e8fb48726267d1a29ff4a77fe5bc1
Nathur64 <36383868+Nathur64@users.noreply.github.com>
2019-04-08 13:41:07 -0500

    Added the method "GetDistance".
    GetDistance would be helpful for developers who want to right features that need to get the distance between players.

--- src/me/chickfla/extrautils/utils/Distance.java
+++ src/me/chickfla/extrautils/utils/Distance.java
@@ -7,4 +7,14 @@ public class Distance {
 	public float DDdistance(double p1_x, double p1_z, double p2_x, double p2_z) {
 		return (float) Math.sqrt( (p2_x-p1_x)*(p2_x-p1_x) + (p2_z-p1_z)*(p2_z-p1_z) );
 	}
+	public float GetDistance(Player p1, Player p2){
+		//Use:   me.chickfla.extrautils.utils:Distance.GetDistance({Player 1}, {Player 2}) ==> {Distance between players}.  
+		float p1X= p1.getLocation().getX();
+		float p1Y= p1.getLocation().getY();
+		float p1Z= p1.getLocation().getZ();
+		float p2X= p2.getLocation().getX();
+		float p2Y= p2.getLocation().getY();
+		float p2Z= p2.getLocation().getZ();
+		return this.DDDdistance(p1X, p1Y, p1Z, p2X, p2Y, p2Z);
+	};
 }
